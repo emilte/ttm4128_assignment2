6b966b2cfad39098edc6ebe5cfd5fbbb057c9bb2
emilte <emil.telstad@gmail.com>
2019-11-06 14:31:24 +0100

    Sync

--- wbem/models.py
+++ wbem/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Options(models.Model):
+    server_url = models.URLField(null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = "options"
--- wbem/views.py
+++ wbem/views.py
@@ -1,10 +1,32 @@
 from django.shortcuts import render
 from django.views import View
+import pywbem
+import json
+from wbem.models import *
 
 # Create your views here.
 class Dashboard(View):
     template = 'wbem/dashboard.html'
 
     def get(self, request):
+
         data = ["Linux", "v2", "76345"] * 3
-        return render(request, self.template, {"data": data})
+
+        # server_url = 'http://ttm4128.item.ntnu.no:5988/root/cimv2'
+        #server_url = Options.objects.first().server_url
+        server_url = 'http://ttm4128.item.ntnu.no:5988/cimom'
+
+        conn = pywbem.WBEMConnection(server_uri)
+
+        # server = pywbem.WBEMServer(conn)
+
+        classnames = conn.EnumerateClassNames(DeepInheritance=False)
+        print(classnames)
+
+        instances = conn.EnumerateInstances('CIM_Processor')
+
+
+        return render(request, self.template, {
+            "data": data,
+            "classnames": classnames,
+        })
