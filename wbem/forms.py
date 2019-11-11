aebc1ea10ef3091452bf8d6b9792e194911a0be6
emilte <emil.telstad@gmail.com>
2019-11-12 00:05:31 +0100

    Remove old code

--- wbem/forms.py
+++ wbem/forms.py
@@ -1,24 +1,4 @@
 from django import forms
-from pywbem import WBEMConnection
-
-
-
-class ClassForm(forms.Form):
-
-    classname = forms.CharField(max_length=200, required=False)
-    #namespace = forms.CharField(max_length=200, required=False)
-    #instance = forms.CharField(max_length=200, required=False)
-    #other_var = forms.CharField(max_length=200, required=False)
-
-    function_choices = [
-        (1, 'EnumerateClassNames'),
-    ]
-    function = forms.ChoiceField(choices=function_choices)
-
-    def __init__(self, *args, **kwargs):
-        super(ClassForm, self).__init__(*args, **kwargs)
-        for field in self.fields.values():
-            field.widget.attrs.update({'class': 'form-control'})
 
 class EiForm(forms.Form):
     # Order is important
